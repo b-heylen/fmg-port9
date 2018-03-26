54a03e3a4b7a21181be22465e2851cb7360d6418
Bert Heylen <bert.heylen@gmail.com>
2018-03-26 14:15:20 +0200

    initial commit

---
+++ run.py
@@ -0,0 +1,46 @@
+from ftntlib.ftntlib import FortiManagerJSON
+import json
+import ssl
+import sys
+import getopt
+import getpass
+import base64
+
+devices = []
+output = "NAME;IP;MASK"
+
+hostname = "151.162.51.130"
+username = "a59313"
+password = "Pegasu9!Pegasu9!"
+adom = "NGN_Direct-Internet-Egress_5-4"
+
+fmg = FortiManagerJSON()
+response = fmg.login(hostname, username, password, ssl=True)
+
+url = 'cli/global/system/global'
+response = fmg._do('get',url)
+wsMode  = response[1]["workspace-mode"]
+
+
+url = '/dvmdb/adom/' + adom + '/device'
+response = fmg.get(url, data="")
+
+data = response[1]
+
+for device in data:
+  devices.append(device["name"])
+
+
+for device in devices:
+  url = 'pm/config/device/' + device + '/global/system/interface/port9'
+  response = fmg.get(url, data="")
+
+  addr = response[1]["ip"][0]
+  mask = response[1]["ip"][1]
+
+  output = output + "\n" + device + ";" + addr + ";" + mask
+
+print("All port9 IP addresses:")
+print(output)
+
+fmg.logout()
